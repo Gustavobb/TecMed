2044d354926b4d6270d16a54166b88afd61fca98
Pedro Fontes <pvfontes1000@gmailcom>
2019-10-31 07:44:42 -0300

    minor fix

--- frontend/tecmed/src/App.js
+++ frontend/tecmed/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
-import Home from './components/Home'
+import Home from './components/Home.js'
+import Review from './components/Review.js'
 
 
 const App = () => {
