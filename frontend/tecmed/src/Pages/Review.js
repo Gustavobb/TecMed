8eecd4fb7d5187248c9c05ff3f0da3d04a236c89
Pedro Fontes <pvfontes1000@gmailcom>
2019-10-31 16:13:51 -0300

    começo dos padrões e pg inicial

--- frontend/tecmed/src/Pages/Review.js
+++ frontend/tecmed/src/Pages/Review.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Review = ({match}) => {
     return(
         <div>
+            <h1>Ola {match.params.usr}</h1>
             <h1>Video: {match.params.id}</h1>
             <p>vamos agora avaliar esse vídeo</p>
         </div>
