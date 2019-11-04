1a549aabf675978b82f22a60932f5549a1a850f0
Mayra Peter <mayra11peter@gmail.com>
2019-11-04 10:39:11 -0300

    front login

--- frontend/tecmed/src/App.js
+++ frontend/tecmed/src/App.js
@@ -5,7 +5,11 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Pages/Home.js'
 import Review from './Pages/Review.js'
 import Quiz from './Pages/Quiz.js'
-
+import Profile from './Components/profile'
+import Landing from './Components/Landing'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import Navbar from './Components/Navbar'
 
 const App = () => {
     return(
@@ -15,6 +19,16 @@ const App = () => {
                     <Route path='/' exact component={Home}/>
                     <Route path='/review/Quiz/' component={Quiz}/>
                     <Route path='/review/VideoId=:id&usr=:usr' exact component={Review}/>
+                    <div className="App">
+                        <Navbar/>
+                        <Route exact path='/teste' component={Landing}/>
+                        <div className="container">
+                            <Route exact path='/teste/register' component={Register}/>
+                            <Route exact path='/teste/login' component={Login}/>
+                            <Route exact path='/teste/profile' component={Profile}/>
+                        </div>
+                    </div>
+                    
                 </Switch>
             </Router>
 
