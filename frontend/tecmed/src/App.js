71b2d6d19919b89692c46bf31b7f490777d2f586
Leo <leonardom1808@gmail.com>
2019-11-04 10:04:31 -0300

    Fix

--- frontend/tecmed/src/App.js
+++ frontend/tecmed/src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Pages/Home.js'
 import Review from './Pages/Review.js'
 import Quiz from './Pages/Quiz.js'
-
+import QuizUser from './Pages/QuizUser.js'
 
 const App = () => {
     return(
@@ -15,7 +15,7 @@ const App = () => {
                     <Route path='/' exact component={Home}/>
                     <Route path='/review/Quiz/' component={Quiz}/>
                     <Route path='/review/VideoId=:id&usr=:usr' exact component={Review}/>
-                    <Route path='/test/:id' exact component={Quiz}/>
+                    <Route path='/test/:id' exact component={QuizUser}/>
                 </Switch>
             </Router>
 
