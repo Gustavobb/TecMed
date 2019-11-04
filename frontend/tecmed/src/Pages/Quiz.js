e88f302b2f499980ff13c605a75a109cb1eff46c
Pedro Fontes <pvfontes1000@gmailcom>
2019-11-04 11:54:55 -0300

    pedro v2.4

--- frontend/tecmed/src/Pages/Quiz.js
+++ frontend/tecmed/src/Pages/Quiz.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/Review.css'
 import PerguntaReview from '../Components/PerguntaReview.js'
+import {Link} from 'react-router-dom';
+
 
 const Quiz = () => {
 
@@ -18,16 +20,30 @@ const Quiz = () => {
     const r3 = getUrlParameter('resposta3');
 
     console.log(r1, r2, r3)
-    
-    return(
-        <div className='Review'>
-            <h1>sente só:</h1>
-            <p>
-                resposta1: <h2>{r1}</h2> resposta2: <h2>{r2}</h2> resposta3: <h2>{r3}</h2>
-            </p>
-            <h1 method='get'></h1>
-        </div>
-    );
+    console.log(r1 < 5, r2 < 5, r3 < 5)
+
+    if(r1 < 5 || r2 < 5 || r3 < 5){
+        return(
+            <div className='Review'>
+                <Link to='/' style={{textDecoration: 'none'}}>
+                    <h2>voltar para home</h2>
+                </Link>
+                <h1>Obrigado pela avaliação!</h1>
+                <p className='texto1'>O vídeo foi devidamente avaliado e não será adicionado ao site.</p>
+            </div>
+        );
+    }
+    else{
+        return(
+            <div className='Review'>
+                <h1>Obrigado pela avaliação!</h1>
+                <p>
+                    resposta1: <h2>{r1}</h2> resposta2: <h2>{r2}</h2> resposta3: <h2>{r3}</h2>
+                </p>
+                <h1 method='get'></h1>
+            </div>
+        );
+    }
 }
 
 
