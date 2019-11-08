60ba33ac544c6545cde2236cd65c7c723b36f9d9
Leo <leonardom1808@gmail.com>
2019-11-07 21:25:37 -0300

    Conectei com o Atlas e estou arrumando a QuizController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,14 +8,15 @@ const QuizController = require('./app/controllers/QuizController')
 const AwsController = require('./app/controllers/AwsController');
 const LoginController = require('./app/controllers/LoginController');
 
-routes.get('/deleteBucket', AwsController.deleteBucket);
-routes.post('/listBuckets', AwsController.listBuckets);
-routes.put('/listObjects', AwsController.listObjects);
-routes.delete('/createBucket', AwsController.createBucket);
-routes.delete('/addObjectToBucket', AwsController.addObjectToBucket);
-routes.delete('/deleteObjectInBucket', AwsController.deleteObjectInBucket);
-routes.get('/quiz', QuizController.sendQuiz)
+//routes.get('/deleteBucket', AwsController.deleteBucket);
+//routes.post('/listBuckets', AwsController.listBuckets);
+//routes.put('/listObjects', AwsController.listObjects);
+//routes.delete('/createBucket', AwsController.createBucket);
+//routes.delete('/addObjectToBucket', AwsController.addObjectToBucket);
+//routes.delete('/deleteObjectInBucket', AwsController.deleteObjectInBucket);
+routes.get('/quiz', QuizController.getQuiz)
 routes.post('/register', LoginController.register);
 routes.post('/login',LoginController.login);
+routes.post('/add', QuizController.addQuiz)
 
 module.exports = routes;
