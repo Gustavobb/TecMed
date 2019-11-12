929158a30f397f3329a6a9f64417ba65061d6e40
slimkaki <rafaalmada98@gmail.com>
2019-11-12 08:39:50 -0300

    metodo para dar update no quiz do video no mongo

--- backend/src/app/controllers/ContentController.js
+++ backend/src/app/controllers/ContentController.js
@@ -21,8 +21,16 @@ class ContentController {
         })
     }
 
-    async getContentQuiz(req, res) {
-        // Pega quiz do vídeo
+    async upodateVideoQuiz(req, res) {
+        var model = new videoModel(req.body)
+        const id = req.query.id // get video id on the query
+        model.findById(id, (err, data)=>{
+            if (err) {console.log('Erro ao salvar o quiz: ' + err);}
+            else{
+                model.update(req.body.quiz);
+                await model.save();
+            };
+        })
     }
 }
 
