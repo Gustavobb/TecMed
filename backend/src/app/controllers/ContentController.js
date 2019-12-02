b9600c06fd0bc48ebf538e0db850b9b38c448898
fontes99 <pvfontes1000@gmail.com>
2019-12-02 16:38:49 -0300

    getunrev vindo porem precisa de ajustes

--- backend/src/app/controllers/ContentController.js
+++ backend/src/app/controllers/ContentController.js
@@ -89,7 +89,7 @@ class ContentController {
 
     async getUnreviewedByCategory(req, res) {
         try {
-            const category = req.params.category;
+            const category = req.query.category;
             var model = await ContentModel.find({"videoSpecifications.reviewed": false, "videoSpecifications.category": category})
             res.send(model)
         } catch(e) {
